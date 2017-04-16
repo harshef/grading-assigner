f63994ec1a457058fe30d441f5d7608ff8e493a0
harshef <maahi.puri@gmail.com>
2017-04-17 01:01:35 +0530

    kkj

---
+++ abc.py
@@ -0,0 +1,3 @@
+import requests
+
+requests.post('https://api.twilio.com/2010-04-01/Accounts/ACaf93c44805d9f4e4582c076c56e2c2cf/Messages.json', auth=('ACaf93c44805d9f4e4582c076c56e2c2cf', 'c24ab76ade8dae7e5a61da8c5b9b2035'))
