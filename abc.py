296014c3f01a63801f797e9a12aad304ca1bb224
harshef <maahi.puri@gmail.com>
2017-04-17 01:20:32 +0530

    twillio

--- abc.py
+++ abc.py
@@ -1,3 +1,8 @@
-import requests
+from twilio.rest import Client
 
-requests.post('https://api.twilio.com/2010-04-01/Accounts/ACaf93c44805d9f4e4582c076c56e2c2cf/Messages.json', auth=('ACaf93c44805d9f4e4582c076c56e2c2cf', 'c24ab76ade8dae7e5a61da8c5b9b2035'))
+account = "ACaf93c44805d9f4e4582c076c56e2c2cf"
+token = "c24ab76ade8dae7e5a61da8c5b9b2035"
+client = Client(account, token)
+
+message = client.messages.create(to="+917351819758", from_="+19732504689",
+                                 body="Hello there!")
